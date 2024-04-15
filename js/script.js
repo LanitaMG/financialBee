63a6fb1ba56f9ea99d2f81a49b6a5631229da565
Lanita <anamagri@gmail.com>
2024-04-15 12:58:06 -0300

    Se depuraron errores en prompt. Se generó readme.md

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@ let incomeCategories = ["Sueldo", "Intereses MP", "Otros"];
 let wallets = ["Efectivo", "Mercado Pago", "Crédito Visa Santander"];
 let user = "";
 
-const welcome = `
-¡Bienvenido a Financial Bee!\n
+const welcome = `¡Bienvenido a Financial Bee!\n
 Bienvenido a la aplicación que te permitirá organizar de forma rápida y práctica todos tus movimientos financieros.\n
 Este tutorial te guiará para que puedas:
 * Crear tu nombre de usuario y contraseña
@@ -18,20 +17,19 @@ Este tutorial te guiará para que puedas:
 
 // Función exit: mensaje de despedida de la aplicación
 function exit(result) {
-  console.log("Cierre ", result);
+  console.log("Cierre de la aplicación con resultado: ", result);
   let msg = "";
   if (result === "Success") {
-    msg = `
-    ¡Excelente trabajo ${user}!
-    Nos vemos en la próxima preentrega para empezar a cargar movimientos
+    msg = `¡Excelente trabajo ${user}!
+Nos vemos en la próxima preentrega para empezar a cargar movimientos.
 
-    Financial Bee
+Financial Bee
     `;
   } else {
-    msg = `
-    ¡Nos vemos en la próxima!
+    msg = `El usuario no pudo ser creado, pero puedes volver a intentarlo.
+¡Nos vemos en la próxima!
     
-    Financial Bee
+Financial Bee
     `;
   }
   alert(msg);
@@ -51,41 +49,44 @@ function userAnswerTest(userAnswer, chs) {
 }
 
 // function userAnswerPrompts: administra la interacción con el usuario cuando se solicita el ingreso de un dato por prompt.
-// Gestiona los mensajes inicial y de aviso de error/reintento. Verifica validez del dato y cantidad de intentos.
+// Gestiona los mensajes inicial y avisos de error/reintento. Verifica validez del dato y cantidad de intentos.
 function userAnswerPrompts(msg1, msg2, chs, iter) {
   let userAnswer = prompt(msg1);
   let it = 1;
-  if (userAnswerTest(userAnswer, chs)) {
-    userAnswer = userAnswer.trim();
-    console.log("it ",it,userAnswer);
-  } else {
-    while (!userAnswerTest(userAnswer, chs) && it < iter) {
-      userAnswer = prompt(msg2);
-      console.log("it ", it, userAnswer);
-      it++;
-    }
-    if (it < iter) {
+  if (userAnswer) {
+    if (userAnswerTest(userAnswer, chs)) {
       userAnswer = userAnswer.trim();
+      console.log("it ",it,userAnswer);
     } else {
-      userAnswer = null;
+      while (userAnswer && !userAnswerTest(userAnswer, chs) && it < iter) {
+        userAnswer = prompt(msg2);
+        it++;
+        console.log("it ", it, userAnswer);
+      }
+      if (it < iter && userAnswer) {
+        userAnswer = userAnswer.trim();
+      } else {
+        userAnswer = null;
+      }
     }
+  } else {
+    userAnswer = null
   }
-  console.log("user: ", userAnswer);
+  console.log("User answer: ", userAnswer);
   return userAnswer;
 }
 
 //function createUser: gestiona la creación del nombre de usuario y contraseña.
 function createUser() {
   let createUserSuccess = false;
-  const msg = `
-El primer paso es crear tu usuario y contraseña.
+  const msg = `El primer paso es crear tu usuario y contraseña.
 Cancelar para salir
 `;
   const userChoice = confirm(msg);
   if (userChoice) {
-    const msg1 = "Por favor introduce un nombre de usuario:";
+    const msg1 = `Por favor introduce un nombre de usuario:`
     const msg2 = `El nombre debe poseer al menos 3 letras.
-    Por favor introduce tu nombre de usuario:
+Por favor introduce tu nombre de usuario:
     `;
     const iter = 3;
     const chs = 3;
@@ -107,17 +108,16 @@ Cancelar para salir
 // function createPassword: gestiona la creación de una contraseña y la validación de la reiteración de contraseña.
 function createPassword() {
   let createPasswordSuccess = false;
-  const msg = `
-  ${user}, vamos a proteger tu cuenta con una contraseña.
-  Debe contener al menos 6 caracteres.
+  const msg = `${user}, vamos a proteger tu cuenta con una contraseña.
+Debe contener al menos 6 caracteres.
   
-  Cancelar para salir"
+Cancelar para salir.
   `;
   const userChoice = confirm(msg);
   if (userChoice) {
     const msg1 = `Por favor introduce una contraseña:`;
     const msg2 = `La contraseña debe poseer al menos 6 caracteres.
-    Por favor introduce una nueva contraseña:";
+Por favor introduce una nueva contraseña:";
     `;
     const iter = 3;
     const chs = 6;
@@ -126,13 +126,12 @@ function createPassword() {
       exit("noSuccess");
     } else {
       /* Validación de contraseña*/
-      let password2 = prompt("Por favor, escribe nuevamente la contraseña:");
+      let password2 = prompt(`Por favor, escribe nuevamente la contraseña:`);
       console.log(password, " == ", password2);
       if (password == password2) {
         createPasswordSuccess = true;
       } else {
-        password2 = prompt(
-          "Los valores no coinciden. Por favor escribe nuevamente la contraseña:"
+        password2 = prompt(`Los valores no coinciden. Por favor escribe nuevamente la contraseña:`
         );
         if (password == password2) {
           createPasswordSuccess = true;
@@ -150,24 +149,25 @@ function createPassword() {
 function editArrays(arr, msgInfo, topic) {
   // Mensaje inicial
   let msg = msgInfo;
-  msg += `Los siguientes elementos ya se encuentran en la app:
+  msg += `
+Los siguientes elementos ya se encuentran en la app:
   `;
   arr.forEach((elem) => {
-    msg += " - " + elem + "\n";
+    msg += ` - ${elem}\n`;
   });
   msg += `
-  A continuación podrás eliminar y agregar elementos para que Bee se adapte a tu estilo de vida financiera
-  \n
-  Cancelar para omitir este paso
+A continuación podrás eliminar y agregar elementos para que Bee se adapte a tu estilo de vida financiera
+  
+Cancelar para omitir este paso
   `;
   if (confirm(msg)) {
     //Listar ELEMENTOS predefinidas para conservar o eliminar
     let elementsSelection = [];
     arr.forEach((element) => {
-      msg = `
-    Estás editando: ${topic} 
-    Deseas conservar: ${element}?
-    Aceptar para conservar - Cancelar para eliminar
+      msg = `Estás editando: ${topic} 
+Deseas conservar: ${element}?
+
+Aceptar para conservar - Cancelar para eliminar
     `;
       let userChoice = confirm(msg);
       if (userChoice) {
@@ -176,38 +176,45 @@ function editArrays(arr, msgInfo, topic) {
     });
 
     // Mostrar elementos conservados
-    msg = `
-  Estás editando: ${topic}
-  Estos son los elementos seleccionados:
+    if (elementsSelection.length > 0) {
+    msg = `Estás editando: ${topic}
+Estos son los elementos seleccionados:
   `;
     elementsSelection.forEach((elem) => {
       msg += ` - ${elem}\n`;
     });
-    msg += "\nA continuación podrás agregar elementos.";
+  } else {
+    msg = `Estás editando: ${topic}
+Tu lista de elementos está vacía. Es importante que agregues elementos para poder realizar la correcta carga de elementos.
+  `;
+
+  }
+
+    msg += `
+A continuación podrás agregar elementos.
+    `
     alert(msg);
 
     //Agregar nuevos elementos
-    const msg1 = `
-  Estás editando: ${topic}
-  Por favor ingrese el nombre del nuevo elemento.
-  Si no desea agregar otro elemento presione Cancelar `;
-    const msg2 = `
-  Estás editando: ${topic}
-  El nombre debe poseer al menos 3 letras.
-  Por favor introduce nuevamente un nombre para el elemento:`;
+    const msg1 = `Estás editando: ${topic}
+Por favor ingrese el nombre del nuevo elemento:
+
+Si no desea agregar otro elemento presione Cancelar `;
+    const msg2 = `Estás editando: ${topic}
+El nombre debe poseer al menos 3 letras.
+Por favor introduce nuevamente un nombre para el elemento:`;
     const iter = 2;
     const chs = 3;
     let newElement = userAnswerPrompts(msg1, msg2, chs, iter);
     while (newElement !== null) {
-      newElement =
-        newElement.charAt(0).toUpperCase() + newElement.slice(1).toLowerCase();
+      newElement = newElement.charAt(0).toUpperCase() + newElement.slice(1).toLowerCase();
+      elementsSelection.push(newElement)
       newElement = userAnswerPrompts(msg1, msg2, chs, iter);
     }
     if (newElement == null) {
       // Mostrar elementos conservados
-      msg = `
-  ${topic}
-  Estos son los elementos que tendrás disponible:
+      msg = `${topic}
+Estos son los elementos que tendrás disponible:
   `;
       elementsSelection.forEach((elem) => {
         msg += ` - ${elem}\n`;
@@ -215,7 +222,7 @@ function editArrays(arr, msgInfo, topic) {
       msg += `\nRecuerda que siempre podrás volver a editar esta sección.`;
       alert(msg);
     }
-    return elementsSelection;
+    return elementsSelection.sort();
   } else {
     return arr;
   }
@@ -223,56 +230,59 @@ function editArrays(arr, msgInfo, topic) {
 
 // function editWallets: lanza la función editArrays con los parámetros para Billeteras
 function editWallets() {
-  let msgInfo = `
-  ${user}, vamos a configurar tus billeteras.
-  Las billeteras son los medios de pago y cobro que utilizas habitualmente
-  Estas son las preconfiguradas en la app:
+  console.log("Edición de Billeteras")
+  let msgInfo = `${user}, vamos a configurar tus billeteras.
+Las billeteras son los medios de pago y cobro que utilizas habitualmente.
   `;
   wallets = editArrays(wallets, msgInfo, "BILLETERAS");
+  console.table(wallets)
 }
 
 // function editIncomeCategories: lanza la función editArrays con los parámetros para Categorías de Ingresos
 function editIncomeCategories() {
-  let msgInfo = `
-  ${user}, vamos a asignarle categorías a tus ingresos.
-  Define todas las categorías que necesites para reflejar el origen de tus ingresos.
-  Estas son las categorías ya disponibles en la app:
+  console.log("Edición de categorías de ingresos")
+  let msgInfo = `${user}, vamos a asignarle categorías a tus ingresos.
+Define todas las categorías que necesites para reflejar el origen de tus ingresos.
   `;
   incomeCategories = editArrays(
     incomeCategories,
     msgInfo,
     "CATEGORÍAS DE INGRESO"
   );
+  console.table(incomeCategories)
 }
 
 // function editExpensesCategories: lanza la función editArrays con los parámetros para Categorías de Egresos
 function editExpensesCategories() {
-  let msgInfo = `
-  ${user} ¿Sabes en qué gastas tu dinero?
-  Identificar las categorías que generan mayor egreso es crucial para tu economía. Crea todas las categorías necesarias para futuros análisis.
-  Estas son las categorías ya disponibles en la app:
+  console.log("Edición de categorías de egresos")
+  let msgInfo = `${user} ¿Sabes en qué gastas tu dinero?
+Identificar las categorías que generan mayor egreso es crucial para tu economía. Crea todas las categorías necesarias para futuros análisis.
   `;
   expensesCategories = editArrays(
     expensesCategories,
     msgInfo,
     "CATEGORÍAS DE EGRESOS"
   );
+  console.table(expensesCategories)
 }
+
+
+
 
 function main() {
   alert(welcome);
   if (createUser()) {
+    console.log("Usuario creado con éxito. Se procede a crear contraseña")
     if (createPassword()) {
-      const msg = `
-      ¡Tu usuario ha sido creado con éxito!
+      console.log("Contraseña creada con éxito. Creación de credencial de cuenta exitosa.")
+      const msg = `¡Tu usuario ha sido creado con éxito!
 
-      ${user} te invitamos a ajustar los parámetros de la aplicación para que se adapten a tu vida financiera.
-      
-      ¿Desea continuar con la personalización de la aplicación?"
+Te invitamos a ajustar los parámetros de la aplicación para que se adapten a tu vida financiera.
+¿Desea continuar con la personalización de la aplicación?
       `;
-      let userChoice = confirm(
-      );
+      let userChoice = confirm(msg);
       if (userChoice) {
+        console.log("Usuario decide avanzar con la personalización")
         editWallets();
         editIncomeCategories();
         editExpensesCategories();
