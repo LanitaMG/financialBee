f2e70a55a01244d5ed57926103d1bf49581cd74e
Lanita <anamagri@gmail.com>
2024-05-06 16:51:07 -0300

    Se generaron las funciones en Js para crear interfacer NuevoUsuario y Personalización

--- js/script.js
+++ js/script.js
@@ -20,11 +20,6 @@ class User {
     this.password = password;
     this.email = email;
   }
-
-  // Método que devuelve el nombre de usuario
-  getUserName() {
-    return this.name;
-  }
 }
 
 // Billeteras
@@ -32,23 +27,6 @@ class Wallet {
   constructor(name, type, active) {
     this.name = name;
     this.type = type;
-    this.active = active;
-  }
-
-  getName() {
-    return this.name;
-  }
-  getType() {
-    return this.type;
-  }
-  getStatus() {
-    return this.active;
-  }
-
-  setType(type) {
-    this.type = type;
-  }
-  setStatus(active) {
     this.active = active;
   }
 }
@@ -59,24 +37,10 @@ class movCategory {
     this.type = type;
     this.active = active;
   }
-
-  getName() {
-    return this.name;
-  }
-  getType() {
-    return this.type;
-  }
-
-  getStatus() {
-    return this.active;
-  }
-
-  setStatus(active) {
-    this.active = active;
-  }
 }
 
 // Funciones
+
 // Inicializar local storage
 function initData() {
   let wallets = [];
@@ -109,42 +73,116 @@ function initData() {
   );
 }
 
-// Genera una estructura div-title-about-list-add-exit para las secciones de personalización.
-function userEditContainer(sectionName) {
+function createUserWindow() {
   const divContainer = document.createElement("section");
-  divContainer.classList += "container shadow p-3";
-  divContainer.id = sectionName + "divContainer";
+  divContainer.classList += "container shadow p-3 w-50";
+  divContainer.id = "user-account";
 
   const title = document.createElement("h2");
-  title.id = sectionName + "Title";
+  title.textContent = "Vamos a crear tu cuenta";
 
   const about = document.createElement("p");
-  about.id = sectionName + "About";
+  about.textContent = "Por favor ingresa los siguientes datos:";
 
-  const listDiv = document.createElement("div");
-  listDiv.classList += "p-3";
-  listDiv.id = sectionName + "List";
+  const erAlert = document.createElement("p");
+  erAlert.classList += "text-danger"
+  erAlert.id = "er-text"
+  erAlert.textContent = ""
 
-  const addNewElement = document.createElement("div");
-  addNewElement.id = sectionName + "AddNew";
+  const formContainer = document.createElement("form");
 
-  const exitBtn = document.createElement("button");
-  exitBtn.name = "editExit";
-  exitBtn.addEventListener("click", createUserSetMenu);
-  exitBtn.classList += "btn btn-dark mt-3 p-3 w-50 mx-auto";
-  exitBtn.textContent = "Finalizar";
+  // Nombre
+  const nameDiv = document.createElement("div");
+  nameDiv.classList += "mb-3";
 
-  divContainer.appendChild(title);
-  divContainer.appendChild(about);
-  divContainer.appendChild(listDiv);
-  divContainer.appendChild(addNewElement);
-  divContainer.appendChild(exitBtn);
+  const helpDiv = document.createElement("div");
+  helpDiv.classList += "form-text";
+  helpDiv.id = "user-help"
+  helpDiv.textContent = "Debe contener al menos 3 letras."
+
+  const nameLabel = document.createElement("label");
+  nameLabel.classList += "form-label";
+  nameLabel.for = "user-name"
+  nameLabel.textContent = "Usuario"
+
+  const nameInput = document.createElement("input");
+  nameInput.classList += "form-control";
+  nameInput.id = "user-name";
+  nameInput.type = "text";
+  nameInput.setAttribute("aria-describedby", "user-help")
+
+  nameDiv.append(nameLabel, nameInput, helpDiv)
+
+
+  // Correo electrónico
+  const emailDiv = document.createElement("div");
+  emailDiv.classList += "mb-3";
+
+  const emailLabel = document.createElement("label");
+  emailLabel.classList += "form-label";
+  emailLabel.for = "user-mail"
+  emailLabel.textContent = "Correo electrónico"
+
+  const emailInput = document.createElement("input");
+  emailInput.classList += "form-control";
+  emailInput.id = "user-email";
+  emailInput.type = "email";
+  emailDiv.append(emailLabel, emailInput);
+
+  // Password 1
+
+  const password1Div = document.createElement("div");
+  password1Div.classList += "mb-3";
+
+  const helpPDiv = document.createElement("div");
+  helpPDiv.classList += "form-text";
+  helpPDiv.id = "userp-help"
+  helpPDiv.textContent = "Debe contener al menos 6 letras."
+
+  const password1Label = document.createElement("label");
+  password1Label.classList += "form-label";
+  password1Label.for = "user-password-1"
+  password1Label.textContent = "Contraseña"
+
+
+  const password1Input = document.createElement("input");
+  password1Input.classList += "form-control";
+  password1Input.id = "user-password-1"
+  password1Input.type = "password"
+  password1Input.setAttribute("aria-describedby", "userp-help")
+
+  password1Div.append(password1Label, password1Input, helpPDiv)
+
+  // password 2
+  const password2Div = document.createElement("div");
+  password2Div.classList += "mb-3";
+
+  const password2Label = document.createElement("label");
+  password2Label.classList += "form-label";
+  password2Label.textContent += "Reingrese su contraseña";
+
+  const password2Input = document.createElement("input");
+  password2Input.classList += "form-control";
+  password2Input.id = "user-password-2"
+  password2Input.type = "password"
+
+  password2Div.append(password2Label, password2Input)
+
+
+  const btnNewUser = document.createElement("button");
+  btnNewUser.classList += "btn btn-dark";
+  btnNewUser.id = "btnNewUser";
+  btnNewUser.textContent = "Crear usuario"
+
+  divContainer.append(title, about, erAlert, nameDiv, emailDiv, password1Div, password2Div, btnNewUser);
 
   mainSection.appendChild(divContainer);
 }
 
+
 //function createUser: gestiona la creación del nombre de usuario y contraseña.
 function createUser() {
+  createUserWindow()
   let createUserOK = false;
   const boton = document.getElementById("btnNewUser");
   boton.addEventListener("click", (e) => {
@@ -200,7 +238,38 @@ function createUser() {
   return createUserOK;
 }
 
-// Menú Configuraciones
+
+// Genera una estructura div-title-about-list-add-exit para las secciones de personalización.
+function userEditContainer(sectionName) {
+  const divContainer = document.createElement("section");
+  divContainer.classList += "w-75 shadow p-3 d-flex flex-column text-center container ";
+  divContainer.id = sectionName + "divContainer";
+
+  const title = document.createElement("h2");
+  title.id = sectionName + "Title";
+
+  const about = document.createElement("p");
+  about.id = sectionName + "About";
+
+  const listDiv = document.createElement("div");
+  listDiv.classList += "p-3 align-self-center w-75 mx-auto";
+  listDiv.id = sectionName + "List";
+
+  const addNewElement = document.createElement("div");
+  addNewElement.id = sectionName + "AddNew";
+
+  const exitBtn = document.createElement("button");
+  exitBtn.name = "editExit";
+  exitBtn.addEventListener("click", createUserSetMenu);
+  exitBtn.classList += "btn btn-dark mt-3 mx-auto";
+  exitBtn.textContent = "Finalizar";
+
+  divContainer.append(title, about, listDiv, addNewElement, exitBtn);
+
+  mainSection.appendChild(divContainer);
+}
+
+// Menú: Crea los botones de acceso a las opciones de personalización
 
 function createUserSetMenu() {
   console.log("funcion Createusersetmenu");
@@ -233,7 +302,7 @@ function clearMainSection() {
 function createList(arr, preId) {
   const checkList = document.createElement("div");
   checkList.id = preId + "CheckList";
-  checkList.classList += "row p-3";
+  checkList.classList += "p-3 ";
   arr.forEach((element, index) => {
     const checkbox = document.createElement("input");
     checkbox.id = preId + "-" + index;
@@ -244,24 +313,22 @@ function createList(arr, preId) {
 
     const labelName = document.createElement("label");
     labelName.textContent = element.name;
-    labelName.classList += "form-check-label col-4";
+    labelName.classList += "form-check-label px-3 col-5";
 
     const labelType = document.createElement("label");
     labelType.textContent = element.type;
-    labelType.classList += "form-label col-4 ";
+    labelType.classList += "form-label col-5";
 
     const listItem = document.createElement("div");
-    listItem.classList += "row";
-    listItem.appendChild(checkbox);
-    listItem.appendChild(labelName);
-    listItem.appendChild(labelType);
+    listItem.classList += "row border-bottom";
+    listItem.append(checkbox, labelName, labelType);
     checkList.appendChild(listItem);
   });
 
   const editBtn = document.createElement("button");
   editBtn.id = preId + "EditBtn";
   editBtn.textContent = "Guardar modificaciones";
-  editBtn.classList += "btn btn-dark m-3 w-50 mx-auto";
+  editBtn.classList += "btn btn-dark m-3";
   checkList.appendChild(editBtn);
 
   return checkList;
@@ -455,16 +522,15 @@ function editIC() {
 
 // Función principal que controla la secuencia de pasos del tutorial.
 function main() {
-  initData();
   const lsUser = JSON.parse(localStorage.getItem("user"));
   console.log(lsUser);
   if (lsUser === null) {
+    initData();
     createUser();
   } else {
     const helloUser = document.getElementById("hello-user");
     helloUser.innerText = "Hola " + lsUser.name;
     const divUserAccount = document.getElementById("user-account");
-    divUserAccount.remove();
     createUserSetMenu();
   }
 }
